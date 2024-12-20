92ffa082899109d89a855f0987c30022eb791060
John Josef <john.josef@gmail.com>
2024-12-20 10:09:02 -0500

    publishing website to s3

--- projects/web/terraform/terraform.tf
+++ projects/web/terraform/terraform.tf
@@ -10,11 +10,93 @@ terraform {
   }
 }
 
-# TODO:
-# s3 bucket and upload dist folder contents, s3 bucket website config, route53 zone and record
-
 module "dir" {
   source  = "hashicorp/dir/template"
   version = "1.0.2"
   base_dir = abspath("${path.module}/../dist")
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.s3_bucket_name
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_acl" "website_bucket_acl" {
+  bucket = aws_s3_bucket.website_bucket.id
+  acl    = "public-read"
+  depends_on = [aws_s3_bucket_public_access_block.frontend_public_access_block, aws_s3_bucket_ownership_controls.ownership_controls_config_bucket]
+}
+
+resource "aws_s3_bucket_versioning" "website_bucket_versioning" {
+  bucket = aws_s3_bucket.website_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend_public_access_block" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "ownership_controls_config_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
+resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": "s3:GetObject",
+        "Resource": "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}/*"
+      }
+    ]
+  }
+  POLICY
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.frontend_public_access_block,
+  ]
+}
+
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+resource "aws_s3_object" "website_files" {
+  for_each = module.dir.files
+
+  bucket       = aws_s3_bucket.website_bucket.id
+  key          = each.key
+  content_type = each.value.content_type
+
+  source  = each.value.source_path
+
+  etag = each.value.digests.md5
+}
+
+output "website_url" {
+  value = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
 }
