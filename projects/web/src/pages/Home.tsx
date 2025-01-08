607b67844af75d0f6026945248aa0cff47e359ed
John Josef <john.josef@gmail.com>
2025-01-08 09:21:51 -0500

    slight adjustment to home logic and added a button

--- projects/web/src/pages/Home.tsx
+++ projects/web/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { SideBar } from '@/components';
+import { Link, SideBar } from '@/components';
 import { getRandomStatus } from '@/lib/statuses';
 import { useEffect, useState } from 'react';
 
@@ -6,10 +6,14 @@ export const Home = () => {
   const [status, setStatus] = useState(getRandomStatus());
 
   useEffect(() => {
-    const iid = setInterval(() => setStatus(getRandomStatus()), 2000);
+    const iid = setTimeout(() => setStatus(getRandomStatus()), 5000);
 
-    return () => clearInterval(iid);
-  }, []);
+    return () => clearTimeout(iid);
+  }, [status]);
+
+  const skipStatus = () => {
+    setStatus(getRandomStatus());
+  };
 
   return (
     <div className="flex flex-row">
@@ -18,6 +22,14 @@ export const Home = () => {
           <code>
             const john = '<code className="text-green-500">{status}</code>';
           </code>
+          <div className="my-2">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-1 px-4 rounded-full"
+              onClick={() => skipStatus()}
+            >
+              <small>Skip</small>
+            </button>
+          </div>
         </div>
       </div>
       <SideBar />
